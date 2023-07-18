53a05a88d3c72ebc56495d8365af52cafa0ff93c
Joey Schentrup <j03y@live.com>
2023-07-17 20:35:09 -0400

    Bootstrap and login modal

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
     import { currentUser, pb } from './pocketbase';
+    import { 
+        Button, 
+        Form, 
+        FormGroup, 
+        Input, 
+        Label,
+        Modal,
+        ModalBody,
+        ModalFooter,
+        ModalHeader
+    } from 'sveltestrap';
+
+    let open = false;
+    const toggle = () => (open = !open);
 
     let username: string;
     let password: string;
@@ -29,17 +43,33 @@
 
 </script>
 
-{#if $currentUser}
-    <p>Logged in as {$currentUser.username}</p>
-    <button on:click={signOut}>Logout</button>
-{:else}
-    <form on:submit|preventDefault>
-
-        <input type="text" bind:value={username} placeholder="Username" />
-        <input type="password" bind:value={password} placeholder="Password" />
-
-        <button on:click={login}>Login</button>
-        <button on:click={register}>Register</button>
-
-    </form>
-{/if}
+<div>
+    <Button color="danger" on:click={toggle}>Open Modal</Button>
+    <Modal isOpen={open} {toggle}>
+      <ModalHeader {toggle}>Modal title</ModalHeader>
+      <ModalBody>
+        {#if $currentUser}
+            <p>Logged in as {$currentUser.username}</p>
+        {:else}
+            <Form on:submit={login}>
+                <FormGroup>
+                    <Label for="usernameInput">Username</Label>
+                    <Input type="text" id="usernameInput" bind:value={username} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="passwordInput">Password</Label>
+                    <Input type="password" id="passwordInput" bind:value={password} />
+                </FormGroup>
+            </Form>
+        {/if}
+      </ModalBody>
+      <ModalFooter>
+        {#if $currentUser}
+            <Button on:click={signOut}>Logout</Button>
+        {:else}
+            <Button color="primary" on:click={login}>Login</Button>
+        {/if}
+        <Button color="secondary" on:click={toggle}>Cancel</Button>
+      </ModalFooter>
+    </Modal>
+  </div>
