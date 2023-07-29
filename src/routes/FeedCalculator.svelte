a089b715fd0619c4ed7d7688538871fd62990a76
Joey Schentrup <j03y@live.com>
2023-07-29 18:38:35 -0400

    Add age and weight to form

--- src/routes/FeedCalculator.svelte
+++ src/routes/FeedCalculator.svelte
@@ -7,6 +7,12 @@
     Input,
     Label,
     Alert,
+    Spinner,
+    InputGroupText,
+    InputGroup,
+    Container,
+    Row,
+    Col,
   } from "sveltestrap";
   import { pb } from "../lib/pocketbase";
   import { onMount } from 'svelte';
@@ -68,6 +74,96 @@
     }
   }
 
+  let age = null;
+  let ageUnits = 'weeks';
+
+  function calcMinAge() {
+    return calcAge(Math.min);
+  }
+
+  function calcMaxAge() {
+    return calcAge(Math.max);
+  }
+
+  function calcAge(reduce: { (...values: number[]): number }) {
+    if (feeds.length === 0) return 0;
+
+    const numDays = feeds.map(feed => {
+      switch (feed.age_units) {
+        case 'days':
+          return feed.age;
+        case 'weeks':
+          return feed.age * 7;
+        case 'months':
+          return feed.age * 30;
+        case 'years':
+          return feed.age * 365;
+      }
+    }).reduce((a, b) => reduce(a, b));
+
+    switch (ageUnits) {
+      case 'days':
+        return numDays;
+      case 'weeks':
+        return numDays / 7;
+      case 'months':
+        return numDays / 30;
+      case 'years':
+        return numDays / 365;
+    }
+  }
+
+  function validateAge() {
+    if (age == null) return;
+
+    if (age < calcMinAge()) {
+      age = calcMinAge();
+    } else if (age > calcMaxAge()) {
+      age = calcMaxAge();
+    }
+  }
+
+  let weight = null;
+  let weightUnits = 'pounds';
+
+  function calcMinWeight() {
+    return calcWeight(Math.min);
+  }
+
+  function calcMaxWeight() {
+    return calcWeight(Math.max);
+  }
+
+  function calcWeight(reduce: { (...values: number[]): number }) {
+    if (feeds.length === 0) return 0;
+
+    const weightPounds = feeds.map(feed => {
+      switch (feed.animal_weight_units) {
+        case 'pounds':
+          return feed.animal_weight;
+        case 'kilograms':
+          return feed.animal_weight * 2.205;
+      }
+    }).reduce((a, b) => reduce(a, b));
+
+    switch (weightUnits) {
+        case 'pounds':
+          return weightPounds;
+        case 'kilograms':
+          return weightPounds / 2.205;
+      }
+  }
+
+  function validateWeight() {
+    if (weight == null) return;
+
+    if (weight < calcMinWeight()) {
+      weight = calcMinWeight();
+    } else if (weight > calcMaxWeight()) {
+      weight = calcMaxWeight();
+    }
+  }
+
   function addToCart() {
     console.log("add to cart");
   }
@@ -79,29 +175,83 @@
   <h1>Feed Calculator</h1>
   <h3>Use this tool to calculate how much feed you need to order.</h3>
   <hr />
-  {#if alertMessage}
-    <Alert color="danger" toggle={() => alertMessage = ''}>
-      {alertMessage}
-    </Alert>
-  {/if}
-  <Form on:submit={addToCart}>
-    <FormGroup>
-      <Label for="speciesInput">Species</Label>
-      <Input type="select" name="select" id="speciesInput" on:change={onSpeciesChange}>
-        {#each species as speciesItem (speciesItem.id)}
-          <option value={speciesItem.id}>{speciesItem.name}</option>
-        {/each}
-      </Input>
-    </FormGroup>
-    {#if conditions.length > 0}
-      <FormGroup>
-        <Label for="conditionInput">Condition</Label>
-        <Input type="select" name="select" id="conditionInput" on:change={onConditionChange}>
-          {#each conditions as conditionItem (conditionItem.id)}
-            <option value={conditionItem.id}>{conditionItem.name}</option>
-          {/each}
-        </Input>
-      </FormGroup>
+  <Container>
+    {#if alertMessage}
+      <Alert color="danger" toggle={() => alertMessage = ''}>
+        {alertMessage}
+      </Alert>
     {/if}
-  </Form>
+    <Form on:submit={addToCart}>
+      <Row>
+        {#if species.length === 0}
+          <Spinner />
+        {:else}
+          <FormGroup>
+            <Label for="speciesInput">Species</Label>
+            <Input type="select" name="Species Input" id="speciesInput" on:change={onSpeciesChange} placeholder="Select the species...">
+              {#each species as speciesItem (speciesItem.id)}
+                <option value={speciesItem.id}>{speciesItem.name}</option>
+              {/each}
+            </Input>
+          </FormGroup>
+        {/if}
+      </Row>
+      {#if conditions.length > 0}
+      <Row>
+        <FormGroup>
+          <Label for="conditionInput">Condition</Label>
+          <Input type="select" name="Condition Input" id="conditionInput" placeholder="Select the animals condition..." on:change={onConditionChange}>
+            {#each conditions as conditionItem (conditionItem.id)}
+              <option value={conditionItem.id}>{conditionItem.name}</option>
+            {/each}
+          </Input>
+        </FormGroup>
+      </Row>
+      {/if}
+      {#if feeds.length > 0}
+        <Row>
+          <Col>
+            <FormGroup>
+              <Label for="conditionInput">Age</Label>
+              <InputGroup>
+                <Input 
+                  type="number" 
+                  name="number" 
+                  id="ageNumber" 
+                  placeholder={"Enter in animals age... (between " + calcMinAge() + " and " + calcMaxAge() + ")"}
+                  min={calcMinAge()} 
+                  max={calcMaxAge()}
+                  bind:value={age}
+                  on:change={validateAge}  
+                />
+                <InputGroupText>
+                  Weeks
+                </InputGroupText>
+              </InputGroup>
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="conditionInput">Weight</Label>
+              <InputGroup>
+                <Input 
+                  type="number" 
+                  name="number" 
+                  id="weightNumber" 
+                  placeholder={"Enter in animals weight... (between " + calcMinWeight() + " and " + calcMaxWeight() + ")"}
+                  min={calcMinWeight()} 
+                  max={calcMaxWeight()}
+                  bind:value={weight}
+                  on:change={validateWeight}  
+                />
+                <InputGroupText>
+                  Pounds
+                </InputGroupText>
+              </InputGroup>
+            </FormGroup>
+          </Col>
+        </Row>
+      {/if}
+    </Form>
+  </Container>
 </Card>
